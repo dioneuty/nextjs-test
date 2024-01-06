8594709a041397b2fcab4028009088d292189cbc
dioneuty <1@293f3900>
2024-01-06 16:00:55 +0900

    네비게이션 css 추가

--- app/_components/Nav.tsx
+++ app/_components/Nav.tsx
@@ -3,6 +3,7 @@
 import {usePathname} from "next/navigation";
 import Link from "next/link";
 import {ReactElement} from "react";
+import styles from './nav.module.css';
 
 export default function Nav(): ReactElement {
 
@@ -10,8 +11,8 @@ export default function Nav(): ReactElement {
 
     return (
         <div className='bg-gray-300 p-5 flex gap-3'>
-            <Link href="/" style={{color: pathname === '/' ? 'red' : 'blue'}} className='bg-blue-300 p-2 rounded'>Home</Link>
-            <Link href="/about" style={{color: pathname === '/about' ? 'red' : 'blue'}}>About</Link>
+            <Link href="/" style={{color: pathname === '/' ? styles.active: ''}} className='bg-blue-300 p-2 rounded'>Home</Link>
+            <Link href="/about" style={{color: pathname === '/about' ? styles.active : ''}}>About</Link>
         </div>
     )
 }
