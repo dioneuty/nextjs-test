0ac7d5fcf91cc0c6d117766c01d87e67aca021be
dioneuty <1@293f3900>
2024-01-06 15:46:35 +0900

    현재 url에 따른 링크 색깔 변경 로직 추가

--- app/_components/Nav.tsx
+++ app/_components/Nav.tsx
@@ -1,11 +1,17 @@
+'use client';
+
+import {usePathname} from "next/navigation";
 import Link from "next/link";
 import {ReactElement} from "react";
 
 export default function nav(): ReactElement {
+
+    const pathname = usePathname();
+
     return (
-        <div>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
+        <div className='bg-gray-300 p-5 flex gap-3'>
+            <Link href="/" style={{color: pathname === '/' ? 'red' : 'blue'}} className='bg-blue-300 p-2 rounded'>Home</Link>
+            <Link href="/about" style={{color: pathname === '/about' ? 'red' : 'blue'}}>About</Link>
         </div>
     )
 }
