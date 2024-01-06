18f0994165ec6cdb71ad6729088bafb237d8f3ee
dioneuty <1@293f3900>
2024-01-06 16:01:44 +0900

    about 페이지 버튼 생성 모듈 css 생성 및 적용

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,14 @@
+import styles from './page.module.css'
+
 export default function about() {
-    return 'about';
+    return (
+        <div>
+            <h1>about</h1>
+            <button className={styles.btn_tomato}>1</button>
+            <button className={styles.btn_tomato}>2</button>
+            <button className={styles.btn_tomato}>3</button>
+            <button className={styles.btn_tomato}>4</button>
+            <button className={styles.btn_tomato}>5</button>
+        </div>
+    );
 }
