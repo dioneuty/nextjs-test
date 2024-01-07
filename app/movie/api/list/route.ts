ee66c052ba8d4cd4735a2ef5f129e5861cda1a80
dioneuty <1@293f3900>
2024-01-07 16:25:10 +0900

    api 키 오프라인 보관

--- app/movie/api/list/route.ts
+++ app/movie/api/list/route.ts
@@ -1,10 +1,12 @@
 import { NextResponse } from "next/server";
 
+const key = process.env.API_KEY;
+
 const options = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYTRjOTk1ODY5YmQxOTNiY2E1OGM1ZjIxMjZjNjU1YyIsInN1YiI6IjYzODRkMmU0MWIxNTdkMDA4MjQ1M2Q4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8MLEqYQU0DK11Eksy-0syIA_RJi9S_bL5wlXYtNG3CA'
+        Authorization: `Bearer ${key}`
     }
 };
 
