cd4de5c050518ebd24e7cb5eb45ea767df3359f7
dioneuty <1@293f3900>
2024-01-07 16:24:48 +0900

    redirects,rewrites 옵션 테스트 생성

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const api = '';
+
+const nextConfig = {
+    async redirects(){
+        return [
+            {
+                source : '/movies2'
+                ,destination : '/about'
+                ,permanent : false
+            }
+        ]
+    },
+    async rewrites(){
+        return [
+            {
+                source : '/get-movie'
+                ,destination : '/movie/api'
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
