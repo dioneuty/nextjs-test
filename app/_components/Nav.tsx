8bae7dcb568ac827e0350a33a56742a74b58682a
dioneuty <1@293f3900>
2024-01-07 14:45:25 +0900

    nav 스타일 수정

--- app/_components/Nav.tsx
+++ app/_components/Nav.tsx
@@ -13,11 +13,15 @@ export default function Nav(): ReactElement {
         return pathname === url && styles.active;
     }
 
+    const cmStyle = 'bg-blue-300 p-2 rounded';
+
     return (
         <div className='bg-gray-300 p-5 flex gap-3'>
-            <Link href="/" className={`${styles.link} ${setActiveColor('/')} bg-blue-300 p-2 rounded`}>Home</Link>
-            <Link href="/about" className={`${styles.link} ${setActiveColor('/about')} && styles.active}`}>About</Link>
-            <Link href="/about" className={'purple'}>Purple</Link>
+            <Link href="/" className={`${styles.link} ${setActiveColor('/')} ${cmStyle}`}>Home</Link>
+            <Link href="/about" className={`${styles.link} ${setActiveColor('/about')} ${cmStyle}`}>About</Link>
+            <Link href="/movie" className={`${styles.link} ${setActiveColor('/movie')} ${cmStyle}`}>Movie</Link>
+            {/*<Link href="/about" className={`${styles.link} ${setActiveColor('/about')} && styles.active}`}>About</Link>
+            <Link href="/movie" className={'purple'}>Movie</Link>*/}
             <style jsx>{`
                 /*div {
                     background-color: purple;
