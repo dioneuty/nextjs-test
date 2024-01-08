47c9dab9a77e8bc733053501bbe9df3826226f97
dioneuty <1@293f3900>
2024-01-09 05:14:58 +0900

    외부 이미지 url 환경설정 추가

--- next.config.js
+++ next.config.js
@@ -19,7 +19,17 @@ const nextConfig = {
                 ,destination : '/movie/api'
             }
         ]
-    }
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'image.tmdb.org',
+                port: '',
+                pathname: '/t/p/w500/**',
+            },
+        ],
+    },
 }
 
 module.exports = nextConfig
