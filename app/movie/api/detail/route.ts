6c76482ea83d26562bb23fc26d82a8cb512b27ef
dioneuty <1@293f3900>
2024-01-09 05:15:32 +0900

    영화 상세정보 api 추가

---
+++ app/movie/api/detail/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+const key = process.env.API_KEY;
+
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${key}`
+    }
+};
+
+export async function GET(request: Request) {
+    const id = request.nextUrl.searchParams.get('id');
+
+    const datsa = await fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US`, options).then(response => response.json()).catch(err => console.error(err));
+
+    return NextResponse.json(datsa);
+}
+
+export async function POST(request: Request) {
+    const res = await request.json();
+    return NextResponse.json({ res });
+}
