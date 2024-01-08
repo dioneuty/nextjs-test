bc203e7be6ad206fa2cbf3ceaa2f45dd31c749a5
dioneuty <1@293f3900>
2024-01-09 05:30:59 +0900

    영화 상세 링크 추가

--- app/movie/movies.tsx
+++ app/movie/movies.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import {useEffect, useState} from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import {useRouter} from "next/router";
 
 export default function Movies () {
 
@@ -18,11 +21,14 @@ export default function Movies () {
     }, []);
 
     return (
-        <div>
-            {!movies && <div>loading...</div>}
-            {movies?.map((movie:{id:number, original_title:string}) => (
-                <div key={movie.id}>
-                    <h4>{movie.original_title}</h4>
+        <div className={`flex flex-wrap`}>
+            {!movies && <div className={`p-5 bg-red-300`}>loading...</div>}
+            {movies?.map((movie:{id:number, original_title:string, poster_path:string}) => (
+                <div key={movie.id} className={`p-4`}>
+                    <Link href={{pathname: `/movie/${movie.id}`, query: {title: movie.original_title,}}}>
+                        <span>{movie.original_title}</span>
+                        <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.original_title} width={300} height={300} />
+                    </Link>
                 </div>
             ))}
         </div>
